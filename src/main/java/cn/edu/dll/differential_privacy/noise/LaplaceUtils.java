8bf92792978cb5d186c834d0a2f3b3be8b99fd21
dulei715 <dulei715@126.com>
2025-11-17 17:33:52 +0800

    Saved

--- src/main/java/cn/edu/dll/differential_privacy/noise/LaplaceUtils.java
+++ src/main/java/cn/edu/dll/differential_privacy/noise/LaplaceUtils.java
@@ -1,12 +1,26 @@
 package cn.edu.dll.differential_privacy.noise;
 
+import cn.edu.dll.basic.random_adapter.RandomGeneratorAdapter;
 import org.apache.commons.math3.distribution.LaplaceDistribution;
+import org.apache.commons.math3.random.RandomAdaptor;
+import org.apache.commons.math3.random.RandomGenerator;
+
+import java.util.Random;
 
 public class LaplaceUtils {
     private LaplaceDistribution laplaceDistribution = null;
 
     public LaplaceUtils(double sensitivity, double epsilon) {
         this.laplaceDistribution = new LaplaceDistribution(0, sensitivity / epsilon);
+    }
+
+    public LaplaceUtils(double sensitivity, double epsilon, RandomGenerator randomGenerator) {
+        this.laplaceDistribution = new LaplaceDistribution(randomGenerator, 0, sensitivity / epsilon);
+    }
+
+    public LaplaceUtils(double sensitivity, double epsilon, Random random) {
+        RandomGenerator randomGenerator = new RandomGeneratorAdapter(random);
+        this.laplaceDistribution = new LaplaceDistribution(randomGenerator, 0, sensitivity / epsilon);
     }
 
     public static double[] getLaplaceNoise(double sensitivity, double epsilon, int number){
